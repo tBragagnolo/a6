199fdae3f3e922846d7f72d60eb64fadc0023c53
tbragagnolo <tbragagnolo@myseneca.ca>
2022-10-01 08:26:01 -0400

    update

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require("express");
+var path = require("path");
 app = express();
 var port = process.env.PORT || 8080;
 
+function onStart(){
+    console.log("Express http server listening on port", port);
+}
+
 app.get("/", (req, res) =>{
-    res.send("./views/home.html");
+    res.sendFile(path.join(__dirname, "/views/home.html"));
 });
 
-app.listen(port);
-console.log("Express http server listening on port", port);
+app.listen(port, onStart);
