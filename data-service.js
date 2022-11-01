38cec497d01778d53290009a87cb0b89136eb585
tbragagnolo <tbragagnolo@myseneca.ca>
2022-11-01 05:05:41 -0400

    update

--- data-service.js
+++ data-service.js
@@ -1,6 +1,31 @@
+var fs = require('fs').promises;
+
 var employees = [];
 var departments = [];
 
-function initialize(){
-    
+exports.initialize = function initialize(){
+    return new Promise(function(resolve, reject){
+        fs.readFile('./data/employees.json',(err,data)=>{
+            if (err) reject("Failure to read file employees.json!");
+            employees = JSON.parse(data);
+        }).then(()=>{
+            fs.readFile('./data/departments.json',(err,data)=>{
+                if (err) reject("Failure to read file departments.json!");
+                departments = JSON.parse(data);
+            }).then(()=>{
+                resolve();
+            });    
+        });
+    });
+}
+
+exports.getAllEmployees = function getAllEmployees(){
+    return new Promise(function(resolve, reject){
+        if(employees.lenght == 0){
+            reject("No results returned");
+        }
+        else{
+            resolve(employees);
+        }
+    });
 }
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var path = require("path");
+var dat = require('./data-service');
 
 app = express();
 var port = process.env.PORT || 8080;
@@ -24,4 +25,12 @@ app.get("/managers", (req, res)=>{
     res.send("<h1>Managers Page</h1>");
 });
 
-app.listen(port, onStart);
+app.use((req, res)=>{
+    res.status(404).send("<h1>Error 404: Page not found</h1>");
+});
+
+dat.initialize().then(()=>{
+    app.listen(port, onStart);
+}).catch((message)=>{
+    console.log(message);
+});
