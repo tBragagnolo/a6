6957c28367156bda2b154d4a0a7af127255d218c
tbragagnolo <tbragagnolo@myseneca.ca>
2022-11-15 05:45:40 -0500

    update

--- server.js
+++ server.js
@@ -39,8 +39,16 @@ app.engine(".hbs", exphbs.engine({
         navLink: function(url, options){
             return '<li' + 
             ((url == app.locals.activeRoute) ? ' class="active" ' : '') + 
-           3
             '><a href=" ' + url + ' ">' + options.fn(this) + '</a></li>';
+        }, 
+        equal: function (lvalue, rvalue, options) {
+            if (arguments.length < 3)
+            throw new Error("Handlebars Helper equal needs 2 parameters");
+            if (lvalue != rvalue) {
+            return options.inverse(this);
+            } else {
+            return options.fn(this);
+            } 
         } 
     }
 }));
