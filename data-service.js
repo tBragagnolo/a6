48ca8123d0a53f37c8fcef1349cf1a2b1e4a365e
tbragagnolo <tbragagnolo@myseneca.ca>
2022-11-28 05:38:25 -0500

    update

--- data-service.js
+++ data-service.js
@@ -1,151 +1,61 @@
-var fs = require('fs');
 
-var employees = [];
-var departments = [];
 
 exports.initialize = function initialize(){
     return new Promise(function(resolve, reject){
-        fs.readFile('./data/employees.json', 'utf8', (err,data)=>{
-            if (err) reject("Failure to read file employees.json!");
-            else{
-                employees = JSON.parse(data);
-
-                fs.readFile('./data/departments.json', 'utf8', (err, data)=>{
-                    if(err) reject("Failure to read file from departments.json");
-                    else{
-                        departments = JSON.parse(data);
-                        resolve();
-                    }
-                });
-            }
-        });
+        reject();
     })
 }
 
 exports.getAllEmployees = function getAllEmployees(){
     return new Promise(function(resolve, reject){
-        if(employees.length == 0){
-            reject("No results returned");
-        }
-        else{
-            resolve(employees);
-        }
+        reject();
     });
 }
 
 exports.getManagers = function getManagers(){
     return new Promise(function(resolve, reject){
-        var managers = [];
-
-        if(employees.length != 0){
-            for(var i = 0; i < employees.length; i++){
-                if(employees[i].isManager == true){
-                    managers.push(employees[i]);
-                }
-            }
-        }
-
-        if(managers.length == 0){
-            reject("No results returned");
-        }
-        else{
-            resolve(managers);
-        }
+        reject();
     });
 }
 
 exports.getDepartments = function getDepartments(){
     return new Promise(function(resolve, reject){
-        if(departments.length == 0){
-            reject("No results returned");
-        }
-        else{
-            resolve(departments);
-        }
+        reject();
     });
 }
 
 exports.addEmployee = function addEmployee(employeeData){
     return new Promise(function(resolve, reject){
-        if(employeeData.isManager == undefined) employeeData.isManager = false;
-        else employeeData.isManager = true;
-        employeeData.employeeNum = employees.length + 1;
-        employees.push(employeeData);
-        resolve();
+        reject();
     });
 }
 
 exports.getEmployeesByStatus = function getEmployeesByStatus(status){
     return new Promise(function(resolve, reject){
-        var emp_s = [];
-
-        if(employees.length != 0){
-            for(var i = 0; i < employees.length; i++){
-                if(employees[i].status == status) emp_s.push(employees[i]);
-            }
-        }
-
-        if(emp_s.length == 0) reject("No results returned");
-        else resolve(emp_s);
+        reject();
     });
 }
 
 exports.getEmployeesByDepartment = function getEmployeesByDepartment(department){
     return new Promise(function(resolve, reject){
-        var emp_d = [];
-
-        if(employees.length != 0){
-            for(var i = 0; i < employees.length; i++){
-                if(employees[i].department == department) emp_d.push(employees[i]);
-            }
-        }
-
-        if(emp_d.length == 0) reject("No results returned");
-        else resolve(emp_d);
+        reject();
     });
 }
 
 exports.getEmployeesByManager = function getEmployeesByManager(manager){
     return new Promise(function(resolve, reject){
-        var emp_m = [];
-
-        if(employees.length != 0){
-            for(var i = 0; i < employees.length; i++){
-                if(employees[i].employeeManagerNum == manager) emp_m.push(employees[i]);
-            }
-        }
-
-        if(emp_m.length == 0) reject("No results returned");
-        else resolve(emp_m);
+        reject();
     });
 }
 
 exports.getEmployeeByNum = function getEmployeeByNum(num){
     return new Promise(function(resolve, reject){
-        var flag = false;
-
-        if(employees.length != 0){
-            for(var i = 0; i < employees.length; i++){
-                if(employees[i].employeeNum == num){
-                    flag = true;
-                    resolve(employees[i]);
-                }
-            }
-        }
-
-        if(flag == false) reject("No results returned");
+        reject();
     });
 }
 
 exports.updateEmployee = function updateEmployee(employeeData){
     return new Promise(function(resolve, reject){
-        for(var i = 0; i < employees.length; i++){
-            if(employees[i].employeeNum == employeeData.employeeNum){
-                employees[i] = employeeData;
-                break;
-            }
-        }
-
-        resolve();
+        reject();
     });
 }
