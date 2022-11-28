40cf6600486e82aa7e7410d9020a4acb5a0b2c72
tbragagnolo <tbragagnolo@myseneca.ca>
2022-11-28 07:37:04 -0500

    update

--- data-service.js
+++ data-service.js
@@ -8,11 +8,41 @@ var sequelize = new Sequelize('ykncfxty', 'ykncfxty', 'f08ogSbiRkLJ677KnDlT17cjf
     ssl: true
    },
    query:{raw: true} // update here. You need it.
-   });
+});
 
 sequelize.authenticate().then(()=> console.log('Connection success.'))
 .catch((err)=>console.log("Unable to connect to DB.", err));
 
+var employees = sequelize.define('Project', {
+    employeeNum: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    firstName: Sequelize.STRING,
+    lastName: Sequelize.STRING,
+    email: Sequelize.STRING,
+    SSN: Sequelize.STRING,
+    addressStreet: Sequelize.STRING,
+    addressCity: Sequelize.STRING,
+    addressState: Sequelize.STRING,
+    addressPostal: Sequelize.STRING,
+    maritalStatus: Sequelize.STRING,
+    isManager: Sequelize.BOOLEAN,
+    employeeManagerNum: Sequelize.INTEGER,
+    status: Sequelize.STRING,
+    department: Sequelize.INTEGER,
+    hireDate: Sequelize.STRING
+});
+
+var departments = sequelize.define('Project', {
+    departmentId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    departmentName: Sequelize.STRING
+});
 
 exports.initialize = function initialize(){
     return new Promise(function(resolve, reject){
