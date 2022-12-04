04316ed682298ea54dfaec57ec8012c4c80a2551
tbragagnolo <tbragagnolo@myseneca.ca>
2022-12-04 05:30:01 -0500

    update

--- data-service-auth.js
+++ data-service-auth.js
@@ -2,3 +2,11 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 mongoose.connect("mongodb+srv://dbUser:12312378DBpass!@senecaweb.ecgnt9t.mongodb.net/a6?retryWrites=true&w=majority");
+
+var testSchema = new Schema({
+    "name": String,
+    "number": {
+        "type": Number,
+        "default": 0
+    }
+});
